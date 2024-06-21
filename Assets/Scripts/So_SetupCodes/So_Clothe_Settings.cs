117a19ecc36dee20edacc92b1192dc6bfc7a148f
SoftRiseGames <98230446+SoftRiseGames@users.noreply.github.com>
2024-06-21 15:15:53 +0300

    last

---
+++ Assets/Scripts/So_SetupCodes/So_Clothe_Settings.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Clothe",menuName ="Customizable/Clothe")]
+public class So_Clothe_Settings : ScriptableObject
+{
+    public Sprite clothe;
+    public int CoinBooster;
+    public int ClickBooster;
+}
