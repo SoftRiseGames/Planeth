4c8424aa708f2dad5631c995b73437b1e7782857
SoftRiseGames <98230446+SoftRiseGames@users.noreply.github.com>
2024-11-05 12:55:46 +0300

    last

--- Assets/LavaCodes.cs
+++ Assets/LavaCodes.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using DG.Tweening;
 public class LavaCodes : MonoBehaviour
 {
-    [SerializeField] float[] AlertLevel;
+    [SerializeField] GameObject Lava;
+    [SerializeField] float speed;
+    [SerializeField] CharacterMovement character;
+
+    [SerializeField] List<ObjectAllDatas> AnchorPoints = new List<ObjectAllDatas>();
+
+
+
+
     void Start()
     {
-        
+        speed = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SpeedControl();
     }
+    void SpeedControl()
+    {
+        /*
+        if (character.SpeedMeter - speed > 3 && character.SpeedMeter - speed < 6)
+            Lava.GetComponent<Image>().rectTransform.DOAnchorPosY(AlertLevel[1], .2f);
+        */
+    }
+
+   
+        
 }
+[System.Serializable]
+public class ObjectLocation
+{
+    public List<float> Location;
+}
+[System.Serializable]
+public class ObjectAllDatas
+{
+    public List<ObjectLocation> AllObject;
+}
+
