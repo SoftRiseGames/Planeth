14f4c6aa711e1847c5c0cc026fc65be3e7b46ae5
SoftRiseGames <98230446+SoftRiseGames@users.noreply.github.com>
2024-12-11 15:59:39 +0300

    last

--- Assets/parallax.cs
+++ Assets/parallax.cs
@@ -7,12 +7,32 @@ public class parallax : MonoBehaviour
     public float scrollspeed = 100f;
     private float offset;
     private Material mat;
+
+    bool isParallaxStart;
     void Start()
     {
         mat = GetComponent<Renderer>().material;
     }
+    private void OnEnable()
+    {
+        Controlls.Launch += ParallaxStartControl;
+    }
+    private void OnDisable()
+    {
+        Controlls.Launch -= ParallaxStartControl;
+    }
+    private void Update()
+    {
+        if(isParallaxStart)
+            ParallaxStart();
 
-    void Update()
+    }
+
+    void ParallaxStartControl()
+    {
+        isParallaxStart = true;
+    }
+    void ParallaxStart()
     {
         offset += (Time.deltaTime * scrollspeed) / 10f;
         mat.SetTextureOffset("_MainTex", new Vector2(0, offset));
